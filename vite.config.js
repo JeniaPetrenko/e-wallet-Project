21a3c12efa5b5541035a72cbc0129ea1a67d848c
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-10-28 14:24:23 +0100

    try deploy

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["react-redux"], // Додайте це, щоб Vite міг розпізнати залежність
+      external: ["@reduxjs/toolkit", "react-redux"],
     },
   },
 });
