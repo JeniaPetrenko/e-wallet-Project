05bd6cceebc96339b88e36622be58e0c706e782d
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-10-28 21:44:24 +0100

    fix build config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/e-wallet-Project/",
   plugins: [react()],
   build: {
     outDir: "dist",
+    assetsDir: "assets",
     rollupOptions: {
-      input: "index.html",
+      input: resolve(__dirname, "index.html"),
+      output: {
+        chunkFileNames: "assets/[name]-[hash].js",
+        entryFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]",
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"),
     },
   },
 });
