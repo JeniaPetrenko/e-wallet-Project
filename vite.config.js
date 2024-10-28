f6ba7dc7cb672f59bc710a3fed30b791fd755a91
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-10-28 21:39:41 +0100

    fix configuration

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/e-wallet-Project/", // Ваш базовий URL
+  base: "/e-wallet-Project/",
   plugins: [react()],
   build: {
+    outDir: "dist",
     rollupOptions: {
-      external: ["@reduxjs/toolkit"], // Залиште лише те, що потрібно виключити
+      input: "index.html",
     },
   },
 });
