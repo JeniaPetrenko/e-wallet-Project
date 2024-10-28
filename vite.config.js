e9ad40abf595bdd1c41ab640a32710b95b2e8534
JeniaPetrenko <144125087+JeniaPetrenko@users.noreply.github.com>
2024-10-28 21:46:44 +0100

    Fix path to main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 export default defineConfig({
   base: "/e-wallet-Project/",
   plugins: [react()],
   build: {
     outDir: "dist",
-    assetsDir: "assets",
-    rollupOptions: {
-      input: resolve(__dirname, "index.html"),
-      output: {
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-        assetFileNames: "assets/[name]-[hash].[ext]",
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "src"),
-    },
   },
 });
